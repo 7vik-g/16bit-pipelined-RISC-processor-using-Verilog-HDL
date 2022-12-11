327a4221ae3858433a693adfdce777e88a57c809
7vik-g <97520594+7vik-g@users.noreply.github.com>
2022-12-11 21:12:04 +0530

    Add files via upload

--- program_counter.v
+++ program_counter.v
@@ -4,12 +4,14 @@ module program_counter(
     input clk, reset,
     output reg [12:0] instr_mem_addr,
     input [12:0] PC_load_addr,
-    input load_enable
+    input load_enable,
+    input start,
+    input hlt
     );
     
     always @(posedge clk, negedge reset)
     if(!reset) instr_mem_addr <= 13'b00000_0000_0000;
+    else if(!start) instr_mem_addr <= 13'b00000_0000_0000;
     else if(load_enable) instr_mem_addr <= PC_load_addr;
-    else instr_mem_addr <= instr_mem_addr + 1'b1;           // implementation of up counter
-    
+    else if(!hlt) instr_mem_addr <= instr_mem_addr + 1'b1;           // implementation of up counter
 endmodule
